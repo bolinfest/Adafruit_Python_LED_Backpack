dcb063905ad5c444d59bc0f4dafcaf6be013f55e
Michael Bolin <bolinfest@gmail.com>
2018-02-14 15:57:03 -0800

    Add parens around print call to make it py3 compatible.

--- examples/ex_7segment_clock.py
+++ examples/ex_7segment_clock.py
@@ -13,7 +13,7 @@ segment = SevenSegment.SevenSegment(address=0x70)
 # Initialize the display. Must be called once before using the display.
 segment.begin()
 
-print "Press CTRL+Z to exit"
+print("Press CTRL+Z to exit")
 
 # Continually update the time on a 4 char, 7-segment display
 while(True):
